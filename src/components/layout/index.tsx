7a5f76c54313655f50a7ab013601b1bf986cc5bc
Mateo Mlikota <mateo_mlikota@outlook.com>
2022-12-12 21:06:05 +0100

    Added: react-router dom package and layout header component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+import Header from "./header";
+
 interface LayoutProps {
-    children: React.ReactNode
+    children: React.ReactNode,
 }
 
 /**
@@ -13,6 +15,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({children}: LayoutProps) => {
     return (
         <>
+            <Header/>
             <main>
                 {children}
             </main>
---
+++ src/interfaces/MenuItem.tsx
@@ -0,0 +1,8 @@
+/**
+ * Menu item interface.
+ */
+export default interface MenuItem {
+    id: string,
+    name: string,
+    link: string
+}
